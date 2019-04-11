a22743c07b49b8e22e589d2d15959b02ed9f467e
Tempeus <36718822+Tempeus@users.noreply.github.com>
2019-04-11 14:05:18 -0400

    Update g92_stopwatch.vhd

--- Assignment 2/g92_stopwatch.vhd
+++ Assignment 2/g92_stopwatch.vhd
@@ -2,6 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+-- stopwatch 
 entity g92_stopwatch is
 	Port (start		:in std_logic;
 			stop		:in std_logic;
@@ -18,7 +19,7 @@ end g92_stopwatch;
 
 architecture behaviour of g92_stopwatch is 
 
-	-- 7-segment decoder
+	-- 7-segment decoder import
 	component g92_7_segment_decoder is
 		port( 
 			code 					: in std_logic_vector (3 downto 0);
@@ -26,7 +27,7 @@ architecture behaviour of g92_stopwatch is
 		);
 	end component g92_7_segment_decoder;
 	
-	-- counter
+	-- counter import
 	component g92_counter is
 		Port (
 					enable : in std_logic;
@@ -36,7 +37,8 @@ architecture behaviour of g92_stopwatch is
 					
 				);
 	end component g92_counter;
-	-- clock divider
+	
+	-- clock divider import
 	
 	component g92_clock_divider is
 	Port (
@@ -48,7 +50,7 @@ architecture behaviour of g92_stopwatch is
 		
 	end component g92_clock_divider;
 	
-	-- store the Binary outputs of counters
+	-- Binary outputs of the counters being used
 	signal HEX0_B			: std_logic_vector (3 downto 0);
 	signal HEX1_B			: std_logic_vector (3 downto 0);
 	signal HEX2_B			: std_logic_vector (3 downto 0);
@@ -56,7 +58,7 @@ architecture behaviour of g92_stopwatch is
 	signal HEX4_B			: std_logic_vector (3 downto 0);
 	signal HEX5_B			: std_logic_vector (3 downto 0);
 	
-	-- enable for each time counters
+	-- enables
 	signal enable_0		: std_logic;
 	signal enable_1		: std_logic;
 	signal enable_2		: std_logic;
@@ -64,7 +66,7 @@ architecture behaviour of g92_stopwatch is
 	signal enable_4		: std_logic;
 	signal enable_5		: std_logic;
 	
-	-- reset for each time counters
+	-- resets
 	signal reset_0			: std_logic;
 	signal reset_1			: std_logic;
 	signal reset_2			: std_logic;
@@ -72,33 +74,26 @@ architecture behaviour of g92_stopwatch is
 	signal reset_4			: std_logic;
 	signal reset_5			: std_logic;
 	
-	-- special signal for millisecond counter
-	signal millisecond_enable : std_logic;
 	
-	signal stopwatch_memory_state : std_logic := '0';
-	
+	-- enable for clock divider 
 	signal clock_div_enable : std_logic;
 	
-	--  the logic
-	
-	--hex0 and hex1 represent milliseconds and can run from 00-99
-	--hex2 and hex3 represent seconds and can run from 00-59
-	--hex4,5 represent minutes an drun from 00-59
-	
+
+
 	
 begin
-	--create clock divider
+	--Clock divider
 	Clock_Divider_0: g92_clock_divider PORT MAP (enable => clock_div_enable, reset => reset, clk => clk, en_out => enable_0);
 	
-	--create 6 counters for each digit
-	counter_0: g92_counter PORT MAP(enable => millisecond_enable, reset => reset_0, clk => clk, count => HEX0_B (3 downto 0));
+	--counter for each digit
+	counter_0: g92_counter PORT MAP(enable => enable_0, reset => reset_0, clk => clk, count => HEX0_B (3 downto 0));
 	counter_1: g92_counter PORT MAP(enable => enable_1, reset => reset_1, clk => clk, count =>HEX1_B (3 downto 0));
 	counter_2: g92_counter PORT MAP(enable => enable_2, reset => reset_2, clk => clk, count =>HEX2_B (3 downto 0));
 	counter_3: g92_counter PORT MAP(enable => enable_3, reset => reset_3, clk => clk, count =>HEX3_B (3 downto 0));
 	counter_4: g92_counter PORT MAP(enable => enable_4, reset => reset_4, clk => clk, count =>HEX4_B (3 downto 0));
 	counter_5: g92_counter PORT MAP(enable => enable_5, reset => reset_5, clk => clk, count =>HEX5_B (3 downto 0));
 	
-	-- create 6 decoders for each digit
+	-- decoder for digits
 	
 	decoder_0: g92_7_segment_decoder PORT MAP (code => HEX0_B(3 downto 0), segments => HEX0(6 downto 0));
 	decoder_1: g92_7_segment_decoder PORT MAP (code => HEX1_B(3 downto 0), segments => HEX1(6 downto 0));
@@ -111,16 +106,15 @@ begin
 				
 			if (stop = '0') then
 				clock_div_enable <= '0';
-			elsif falling_edge(start) then
+			elsif rising_edge(start) then
 				clock_div_enable <= '1';
 			end if;
 			
-		millisecond_enable <= enable_0;
-		stopwatch_memory_state <= clock_div_enable;
 		
-		-- set enable/not for each digit to increase
+		-- enable on/off
+		-- enable of current component will go into next component
 		if (HEX0_B = "1001") then
-			enable_1 <= millisecond_enable;
+			enable_1 <= enable_0;
 		else enable_1 <= '0';
 		end if;
 		
@@ -142,7 +136,6 @@ begin
 			enable_4 <= '0';
 		end if;
 		
-		
 	
 		if (HEX4_B = "1001") then
 			enable_5 <= enable_4;
@@ -152,7 +145,7 @@ begin
 		
 		
 		
--- reset hex0 when it becomes greater than 9
+-- reset hex0 if > 9
 		
 		if (HEX0_B = "1010" or reset = '0') then
 			reset_0 <= '0';
@@ -160,7 +153,7 @@ begin
 			reset_0 <= '1';
 		end if;
 		
--- reset hex1 when it becomes greater than 9
+-- reset hex1 if > 9
 		
 		if (HEX1_B = "1010" or reset = '0') then
 			reset_1 <= '0';
@@ -168,7 +161,7 @@ begin
 			reset_1 <= '1';
 		end if;
 		
--- reset hex2 when it becomes greater than 9
+-- reset hex2 if > 9
 		
 		if (HEX2_B = "1010" or reset = '0') then
 			reset_2 <= '0';
@@ -176,7 +169,7 @@ begin
 			reset_2 <= '1';
 		end if;
 		
--- reset hex3 when it becomes greater than 5
+-- reset hex3 if > 5
 		
 		if (HEX3_B = "0110" or reset = '0') then
 			reset_3 <= '0';
@@ -184,7 +177,7 @@ begin
 			reset_3 <= '1';
 		end if;
 		
--- reset hex4 when it becomes greater than 9
+-- reset hex4 if > 9
 		
 		if (HEX4_B = "1010" or reset = '0') then
 			reset_4 <= '0';
